57a75f73b241293c52276fe32cff20dd9ccc599a
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-06-13 18:29:30 +0200

    Create order creation logic

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,9 +1,63 @@
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@vttickets/common';
 import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = Router();
 
-router.post('/api/orders', (req: Request, res: Response) => {
-  res.send({ msg: 'Single order item' });
-});
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be defined'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that the ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = await Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    res.status(200).send(order);
+  }
+);
 
 export { router as newOrderRouter };
