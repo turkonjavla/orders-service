51388b0f253e4fc4f26b4f9b0c40caf32b1e415d
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-06-14 12:38:35 +0200

    Create route handler for fetching all orders of the logged in user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@vttickets/common';
 import { Router, Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({ message: 'Orders index route' });
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('ticket');
+
+  res.status(200).send(orders);
 });
 
 export { router as indexOrderRouter };
