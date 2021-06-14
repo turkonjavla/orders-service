6caf422dfe8adfeb22ccddd46d4176943d732141
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-06-14 18:55:44 +0200

    Add cancel order functionality and tests

--- src/routes/delete.ts
+++ src/routes/delete.ts
@@ -1,9 +1,35 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from '@vttickets/common';
 import { Router, Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = Router();
 
-router.delete('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send({ msg: 'Delete single order' });
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
