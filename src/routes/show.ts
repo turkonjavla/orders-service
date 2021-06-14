df9a580537f00a51ff31a8c97a2ab3c1c273442e
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-06-14 16:34:19 +0200

    Add show route and tests

--- src/routes/show.ts
+++ src/routes/show.ts
@@ -1,9 +1,32 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@vttickets/common';
 import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = Router();
 
-router.get('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send({ msg: 'Single order item' });
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
